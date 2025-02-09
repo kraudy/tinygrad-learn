0d9cff804942da5866c9220dafd768be64ac1456
Roberto Torrez <robkraudy@gmail.com>
2025-02-08 18:49:53 -0600

    Describe simple kernel
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- pattern.py
+++ pattern.py
@@ -1,5 +1,17 @@
-from tinygrad import Tensor
+from tinygrad import Tensor, dtypes#, UPat, Ops
+from tinygrad.ops import UPat, Ops, UOp
+from tinygrad.renderer.cstyle import MetalRenderer
 
-a = Tensor.empty(4,4)
-b = a + 1
-b.realize()
+metal_render = MetalRenderer()
+
+const = UOp(Ops.CONST, dtypes.float, arg=1.0)
+define_global = UOp(Ops.DEFINE_GLOBAL, dtypes.float.ptr(), arg=0)
+special = UOp(Ops.SPECIAL, dtypes.int, arg=('gidx0', 16), src=())
+added = UOp(Ops.ADD, dtypes.long, arg=None, src=(define_global, special))
+store = UOp(Ops.STORE, dtypes.void, arg=None, src=(added, const))
+uops = [const, define_global, special, added, store]
+
+rendered = metal_render.render("rendered", uops)
+print(rendered)
+
+
