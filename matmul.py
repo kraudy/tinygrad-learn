6bc04592631c3e47d458ae0025438991d94218a5
Roberto Torrez <robkraudy@gmail.com>
2025-02-12 07:09:59 -0600

    Document some lazydata
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- matmul.py
+++ matmul.py
@@ -10,11 +10,6 @@ has to have a JIT compiler for the AST to the tensor device; CLANG in this case.
 a = Tensor.empty(4,4)
 ld = a.lazydata
 print(a)
-print(a.lazydata)
-print(a.device)
-print(ld.st if ld.base is not ld else (ld.op, ld.realized))
-print(ld.st)
-print((ld.op, ld.realized))
 """
 <Tensor <UOp CLANG (4, 4) float 
 ShapeTracker(views=(View(shape=(4, 4), strides=(4, 1), 
@@ -23,3 +18,20 @@ offset=0, mask=None, contiguous=True),))> on CLANG with grad None>
 Interesting how the tensor has the ShapeTraker and the target device (CLANG)
 This means, a Tenser needs to have a target device for the AST 
 """
+
+print(a.lazydata)
+"""
+UOp(Ops.VIEW, dtypes.float, arg=ShapeTracker(views=(View(shape=(4, 4), strides=(4, 1), offset=0, mask=None, contiguous=True),)), src=(
+  UOp(Ops.BUFFER, dtypes.float, arg=(2, 16), src=(
+    UOp(Ops.DEVICE, dtypes.void, arg='CLANG', src=()),)),))
+"""
+print(a.device)
+"""CLANG"""
+print(ld.st if ld.base is not ld else (ld.op, ld.realized))
+print(ld.st)
+"""ShapeTracker(views=(View(shape=(4, 4), strides=(4, 1), offset=0, mask=None, contiguous=True),))"""
+print((ld.op, ld.realized))
+"""(<Ops.VIEW: 22>, None)"""
+
+
+
