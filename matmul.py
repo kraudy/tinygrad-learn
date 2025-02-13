f2fa293047df61d3fa4f028403872f01a6785cbb
Roberto Torrez <robkraudy@gmail.com>
2025-02-12 19:38:58 -0600

    Explain matmul logic
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- matmul.py
+++ matmul.py
@@ -65,9 +65,22 @@ So, the same View that we define helps us when we need to do the operations
 """
                 Add new dim   Expand to 3 
 a = (2, 4) --> (2, 1, 4) ----------------> (2, 3, 4)
+Here we are basically expanding the two rows 3 times each to mach the 3 columns of b
+and do a vector dot product between each vector. Note dim 1 (3) is equal b dim 1 (3)
+
                 Add new dim   Transpose
 b = (4, 3) --> (1, 4, 3) --> (1, 3, 4) --> (2, 3, 4)
+Note how we do the transform when the dim0 is 1, so when dim0 is 2, the (3,4) matrix gets expanded and then
+we can do vector by vector dot product
+
+Incrementing dim1, expands dim2
+Incrementing dim0, expands dim1,dim2
+
+At the end, a and b have the same shape but different data. After the reducion
+c = (2, 3). dim2 dissapears
 
 Now to the dot product a@b and get c = (2, 3)
 We got to the operation just manipulating the View, doing a column-wise operation and then a reduce sum
+
+A matrix dot product is just many vector dot product or scalar product where each scalar = (x, y) cell
 """
