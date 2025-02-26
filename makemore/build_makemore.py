13c100b2fe39a2e6e583bc326fc57818324c6ca7
Roberto Torrez <robkraudy@gmail.com>
2025-02-26 12:55:59 -0600

    Make alphabet tensor
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- makemore/build_makemore.py
+++ makemore/build_makemore.py
@@ -1,2 +1,20 @@
-word = open('names.txt', 'r').read().splitlines()
-print(word)
+words = open('names.txt', 'r').read().splitlines()
+print(words[:10])
+print(f"len: {len(words)}")
+print(f"min len word: {min(len(w) for w in words)}")
+print(f"max len word: {max(len(w) for w in words)}")
+
+b = {}
+for w in words[:3]:
+  chs = ['<S>'] + list(w) + ['<E>']
+  for ch1, ch2 in zip(chs, chs[1:]):
+    bigram = (ch1, ch2)
+    b[bigram] = b.get(bigram, 0) + 1
+
+print(b)
+
+from tinygrad import Tensor, nn, TinyJit, dtypes
+
+N = Tensor.empty(28, 28, dtype=dtypes.int32)
+N[1,3] = 1
+print(N.numpy())
