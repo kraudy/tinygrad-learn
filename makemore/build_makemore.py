df1639be0a975496c25b3f9c2c29dce5dabe1ad6
Roberto Torrez <robkraudy@gmail.com>
2025-02-26 13:59:53 -0600

    Change special delimiters to '.'
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- makemore/build_makemore.py
+++ makemore/build_makemore.py
@@ -6,27 +6,29 @@ print(f"max len word: {max(len(w) for w in words)}")
 
 
 chars = sorted(list(set(''.join(words))))
-stoi = {s:i for i,s in enumerate(chars)}
-stoi['<S>'] = 26
-stoi['<E>'] = 27
-
+"""Get alphabet."""
+stoi = {s:i+1 for i,s in enumerate(chars)}
+stoi['.'] = 0
+"""Letters to int. 28 Elements"""
+itos = {i:s for s,i in stoi.items()}
 
 import numpy as np
 
 # Use NumPy array for counting
-N_np = np.zeros((28, 28), dtype=np.int32)
+N_np = np.zeros((27, 27), dtype=np.int32)
+"""27x27 Matrix since we have 27 symbols."""
 
-b = {}
 for w in words:
-  chs = ['<S>'] + list(w) + ['<E>']
+  chs = ['.'] + list(w) + ['.']
   for ch1, ch2 in zip(chs, chs[1:]):
     ix1 = stoi[ch1]
     ix2 = stoi[ch2]
-    #N[ix1, ix2] += 1
     N_np[ix1, ix2] += 1
+    """Get letter occurrence. This looks like a probability distribution over each letter"""
 
 from tinygrad import Tensor, nn, TinyJit, dtypes
 
 N = Tensor(N_np, dtype=dtypes.int32)
 print(N.shape)
 print(N.numpy())
+
