8363b2baea7b4062c65d419c5343a7fb39683f61
Roberto Torrez <robkraudy@gmail.com>
2025-03-02 09:37:16 -0600

    Simple forward with 1 neuron
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- makemore/build_makemore_2.py
+++ makemore/build_makemore_2.py
@@ -27,4 +27,15 @@ for w in words[:1]:
 xs = torch.tensor(xs)
 ys = torch.tensor(ys)
 
-print(xs, ys)
+import torch.nn.functional as F
+
+xenc = F.one_hot(xs, num_classes=27).float()
+"""One hot encoding makes a vector of 27 elements and assign probability on one to represent each element.
+This becomes the linear representation of the input."""
+print(xenc)
+
+W = torch.randn((27, 1))
+print(xenc.shape, W.shape)
+"""(5, 27) x (27, 1) will become (5, 1)"""
+print((xenc @ W).shape)
+print((xenc @ W))
