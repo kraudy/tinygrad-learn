818382ce8944182b6e411f85101d997ebe405b2c
Roberto Torrez <robkraudy@gmail.com>
2025-03-02 09:07:49 -0600

    Prepare inputs to neuron
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- makemore/build_makemore_2.py
+++ makemore/build_makemore_2.py
@@ -10,3 +10,21 @@ itos = {i:s for s,i in stoi.items()}
 import numpy as np
 import torch
 
+N = torch.ones(27, 27)
+"""27x27 Matrix since we have 27 symbols.
+Start each bytegram count as one to not get -inf on log function."""
+
+xs, ys = [], []
+
+for w in words[:1]:
+  chs = ['.'] + list(w) + ['.']
+  for ch1, ch2 in zip(chs, chs[1:]):
+    ix1 = stoi[ch1]
+    ix2 = stoi[ch2]
+    xs.append(ix1)
+    ys.append(ix2)
+  
+xs = torch.tensor(xs)
+ys = torch.tensor(ys)
+
+print(xs, ys)
