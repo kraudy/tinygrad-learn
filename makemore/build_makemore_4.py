66f54c8c5c9b495001118300c0eef1ae11fb1ef6
Roberto Torrez <robkraudy@gmail.com>
2025-03-12 20:19:15 -0600

    Refacto parameters
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- makemore/build_makemore_4.py
+++ makemore/build_makemore_4.py
@@ -28,52 +28,49 @@ Y = torch.tensor(Y)
 print(X); print(X.shape)
 print(Y); print(Y.shape)
 
-C = torch.randn(27, 2)
+g = torch.Generator().manual_seed(2147483647)
+C = torch.randn((27, 2), generator=g)
 """Embedding"""
+W1 = torch.randn((3*2, 100), generator=g)
+"""This means a layer with 100 neurons and 6 weigths per neuron."""
+b1 = torch.randn(100, generator=g)
 
-print(C[5])
-print(C[X].shape)
+W2 = torch.randn((100, 27), generator=g)
+"""This means a layer with 27 neurons and 100 weigths per neuron."""
+b2 = torch.randn(27, generator=g)
+parameters = [C, W1, b1, W2, b2]
+print(sum(p.nelement() for p in parameters))
+"""3481"""
 
-print(X[10])
-"""[ 9, 22,  9]"""
-print(C[X[10]])
 """
+[ 9, 22,  9]
+
 [ [ 1.2605,  0.8640], => 9
   [-1.5719, -0.7918], => 22
   [ 1.2605,  0.8640]  => 9 ]
-"""
-print(C[9]); print(C[22]); print(C[9])
-"""
+
 [ 1.2605,  0.8640], [-1.5719, -0.7918], [ 1.2605,  0.8640]  ]
-Intead of using a one_hot encoding we just index the layer matrix to get out each
-index 2d representation"""
+"""
 
 emb = C[X]
-print(emb.shape)
-"""[32, 3, 2]"""
-W1 = torch.randn(3*2, 100)
-"""This means a layer with 100 neurons and 6 weigths per neuron."""
-b1 = torch.randn(100)
-
-#print(W1)
+"""[32, 3, 2]
+Intead of using a one_hot encoding we just index the layer matrix to get out each
+index 2d representation"""
 
 #(32, 6)
 h = (emb.view(-1,6) @ W1 + b1).tanh()
 """A tensor is basically a 1d vector array with shapes, strides and size that describes a view which 
 determines how the data is represented."""
-print(h.shape)
-"""[32, 100]"""
-
-W2 = torch.randn(100, 27)
-"""This means a layer with 27 neurons and 100 weigths per neuron."""
-b2 = torch.randn(27)
+"""h.shape: [32, 100]"""
 
 logits = (h @ W2 + b2)
-print(logits.shape)
-"""[32, 27]"""
+"""logits.shape: [32, 27]"""
 counts = logits.exp()
 prob = (counts / counts.sum(1, keepdims=True))
-print(prob.shape)
-"""[32, 27]"""
-print(prob[0].sum())
-print(prob[torch.arange(32), Y])
+"""prob.shape: [32, 27]"""
+print(prob[0].sum()); print(prob[torch.arange(32), Y])
+
+loss = - prob[torch.arange(32), Y].log().mean(); print(loss)
+
+
+
