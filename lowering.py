615f5f19f9fed80391e9f0a6e6097360eeaead71
Roberto Torrez <robkraudy@gmail.com>
2025-04-09 07:03:05 -0600

    Adding context
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- lowering.py
+++ lowering.py
@@ -4,12 +4,19 @@ Tensor.manual_seed(42)
 
 n_symbols = 28
 v_size = 3
+context_size = 3
+elements_size = 100
 
-words = Tensor.randint(100, low=0, high=n_symbols-1)
-"""Words as randoms int"""
+X = Tensor.randint(elements_size, context_size, low=0, high=n_symbols-1)
+"""Sequence of context_size words"""
+Y = Tensor.randint(elements_size, low=0, high=n_symbols-1)
+"""Word to predict from context"""
 C = Tensor.randn(n_symbols, v_size)
 """Vector representatin for each word"""
 
-print(C[words[:2]].numpy())
 
+
+print(X[0].numpy())
+print(C[X[0]].numpy())
+print(Y[0].numpy())
 
