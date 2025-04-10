aa3decfcddf1f2de4becba89786b37427ba43ecd
Roberto Torrez <robkraudy@gmail.com>
2025-04-09 19:42:38 -0600

    Doing optimizer
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- lowering.py
+++ lowering.py
@@ -1,4 +1,4 @@
-from tinygrad import Tensor
+from tinygrad import Tensor, nn
 
 Tensor.manual_seed(42)
 
@@ -25,20 +25,32 @@ iter
 """
 
 L1_neurons = 100
-W1 = Tensor.randn(v_size*context_size, L1_neurons, requires_grad=True)
-b1 = Tensor.randn(L1_neurons, requires_grad=True)
+W1 = Tensor.randn(v_size*context_size, L1_neurons)
+b1 = Tensor.randn(L1_neurons)
 
 L2_neurons = n_symbols
-W2 = Tensor.randn(L1_neurons, L2_neurons, requires_grad=True)
-b2 = Tensor.randn(L2_neurons, requires_grad=True)
+W2 = Tensor.randn(L1_neurons, L2_neurons)
+b2 = Tensor.randn(L2_neurons)
 
+parameters = [C, W1, b1, W2, b2]
+lr = 0.1
+optimizer = nn.optim.SGD(parameters, lr)
 
 print(X[0].numpy())
 print(C[X[:2]].flatten(1).numpy())
 print(Y[0].numpy())
 
 #logits = C[X].flatten(1) @ W1 + b1 
+Tensor.training = True
+
 logits = (C[X].flatten(1)).matmul(W1).add(b1).tanh()
-loss = logits
+loss = logits.matmul(W2).add(b2).cross_entropy(Y)
+
+optimizer.zero_grad()
+
+loss.backward()
+# do optimizer
+optimizer.step()
 
 print(logits.numpy())
+print(loss.numpy())
