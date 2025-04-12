251dfcd530b1037501ef57f5b97752b9d02699c2
Roberto Torrez <robkraudy@gmail.com>
2025-04-12 06:51:35 -0600

    Tensor lazy data
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- tracing.py
+++ tracing.py
@@ -92,4 +92,94 @@ tensor.py(190)_apply_uop()
 
 rand()
 
+> /home/kraudy/tinygrad/tinygrad/tinygrad/tensor.py(572)full()-><Tensor <UOp ...ith grad None>
+-> return Tensor(fill_value, **kwargs).reshape((1, )*len(new_shape := argfix(shape))).expand(new_shape)
+
+
+
+"""
+
+
+"""
+Tensor Lazy data, i dont' know why is so big
+
+(Pdb) self.lazydata
+UOp(Ops.ADD, dtypes.uint, arg=None, src=(
+  UOp(Ops.PAD, dtypes.uint, arg=((0, 3),), src=(
+    UOp(Ops.CAST, dtypes.uint, arg=None, src=(
+      UOp(Ops.AND, dtypes.ulong, arg=None, src=(
+        x3:=UOp(Ops.THREEFRY, dtypes.ulong, arg=None, src=(
+          UOp(Ops.OR, dtypes.ulong, arg=None, src=(
+            UOp(Ops.MUL, dtypes.ulong, arg=None, src=(
+              UOp(Ops.CAST, dtypes.ulong, arg=None, src=(
+                UOp(Ops.ADD, dtypes.uint, arg=None, src=(
+                  x8:=UOp(Ops.ADD, dtypes.uint, arg=None, src=(
+                    UOp(Ops.ADD, dtypes.uint, arg=None, src=(
+                      UOp(Ops.RESHAPE, dtypes.uint, arg=(3,), src=(
+                        UOp(Ops.REDUCE_AXIS, dtypes.uint, arg=(Ops.ADD, (1,)), src=(
+                          UOp(Ops.PERMUTE, dtypes.uint, arg=(1, 0), src=(
+                            UOp(Ops.RESHAPE, dtypes.uint, arg=(3, 3), src=(
+                              UOp(Ops.RESHAPE, dtypes.uint, arg=(3, 3, 1), src=(
+                                UOp(Ops.SHRINK, dtypes.uint, arg=((0, 3), (0, 3)), src=(
+                                  UOp(Ops.RESHAPE, dtypes.uint, arg=(3, 6), src=(
+                                    UOp(Ops.SHRINK, dtypes.uint, arg=((0, 18),), src=(
+                                      UOp(Ops.RESHAPE, dtypes.uint, arg=(20,), src=(
+                                        UOp(Ops.EXPAND, dtypes.uint, arg=(4, 5), src=(
+                                          UOp(Ops.RESHAPE, dtypes.uint, arg=(1, 5), src=(
+                                            UOp(Ops.PAD, dtypes.uint, arg=((2, 0),), src=(
+                                              UOp(Ops.EXPAND, dtypes.uint, arg=(3,), src=(
+                                                UOp(Ops.RESHAPE, dtypes.uint, arg=(1,), src=(
+                                                  UOp(Ops.CONST, dtypes.uint, arg=1, src=(
+                                                    x25:=UOp(Ops.VIEW, dtypes.void, arg=ShapeTracker(views=(View(shape=(), strides=(), offset=0, mask=None, contiguous=True),)), src=(
+                                                      x26:=UOp(Ops.DEVICE, dtypes.void, arg='CLANG', src=()),)),)),)),)),)),)),)),)),)),)),)),)),)),)),)),)),
+                      UOp(Ops.EXPAND, dtypes.uint, arg=(3,), src=(
+                        UOp(Ops.RESHAPE, dtypes.uint, arg=(1,), src=(
+                          UOp(Ops.CONST, dtypes.uint, arg=-1, src=(
+                             x25,)),)),)),)),
+                    UOp(Ops.EXPAND, dtypes.uint, arg=(3,), src=(
+                      UOp(Ops.VIEW, dtypes.uint, arg=ShapeTracker(views=(View(shape=(1,), strides=(0,), offset=0, mask=None, contiguous=True),)), src=(
+                        UOp(Ops.COPY, dtypes.uint, arg=False, src=(
+                           x26,
+                          UOp(Ops.BUFFER, dtypes.uint, arg=(1, 1), src=(
+                            x34:=UOp(Ops.DEVICE, dtypes.void, arg='PYTHON', src=()),)),)),)),)),)),
+                  UOp(Ops.EXPAND, dtypes.uint, arg=(3,), src=(
+                    UOp(Ops.RESHAPE, dtypes.uint, arg=(1,), src=(
+                      UOp(Ops.CONST, dtypes.uint, arg=3, src=(
+                         x25,)),)),)),)),)),
+              x38:=UOp(Ops.EXPAND, dtypes.ulong, arg=(3,), src=(
+                UOp(Ops.RESHAPE, dtypes.ulong, arg=(1,), src=(
+                  UOp(Ops.CONST, dtypes.ulong, arg=4294967296, src=(
+                     x25,)),)),)),)),
+            UOp(Ops.CAST, dtypes.ulong, arg=None, src=(
+               x8,)),)),
+          UOp(Ops.OR, dtypes.ulong, arg=None, src=(
+            UOp(Ops.MUL, dtypes.ulong, arg=None, src=(
+              UOp(Ops.CAST, dtypes.ulong, arg=None, src=(
+                UOp(Ops.EXPAND, dtypes.uint, arg=(3,), src=(
+                  UOp(Ops.RESHAPE, dtypes.uint, arg=(1,), src=(
+                    UOp(Ops.RESHAPE, dtypes.uint, arg=(), src=(
+                      UOp(Ops.SHRINK, dtypes.uint, arg=((1, 2),), src=(
+                        x49:=UOp(Ops.VIEW, dtypes.uint, arg=ShapeTracker(views=(View(shape=(2,), strides=(1,), offset=0, mask=None, contiguous=True),)), src=(
+                          UOp(Ops.COPY, dtypes.uint, arg=False, src=(
+                             x26,
+                            UOp(Ops.BUFFER, dtypes.uint, arg=(0, 2), src=(
+                               x34,)),)),)),)),)),)),)),)),
+               x38,)),
+            UOp(Ops.CAST, dtypes.ulong, arg=None, src=(
+              UOp(Ops.EXPAND, dtypes.uint, arg=(3,), src=(
+                UOp(Ops.RESHAPE, dtypes.uint, arg=(1,), src=(
+                  UOp(Ops.RESHAPE, dtypes.uint, arg=(), src=(
+                    UOp(Ops.SHRINK, dtypes.uint, arg=((0, 1),), src=(
+                       x49,)),)),)),)),)),)),)),
+        x57:=UOp(Ops.EXPAND, dtypes.ulong, arg=(3,), src=(
+          UOp(Ops.RESHAPE, dtypes.ulong, arg=(1,), src=(
+            UOp(Ops.CONST, dtypes.ulong, arg=4294967295, src=(
+               x25,)),)),)),)),)),)),
+  UOp(Ops.PAD, dtypes.uint, arg=((3, 0),), src=(
+    UOp(Ops.CAST, dtypes.uint, arg=None, src=(
+      UOp(Ops.AND, dtypes.ulong, arg=None, src=(
+        UOp(Ops.IDIV, dtypes.ulong, arg=None, src=(
+           x3,
+           x38,)),
+         x57,)),)),)),))
 """
