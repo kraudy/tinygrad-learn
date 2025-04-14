ce52d40631469db66852cc05f1ab0b09f63da9c7
Roberto Torrez <robkraudy@gmail.com>
2025-04-14 09:50:04 -0600

    Turning tensors into Uops
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- tracing_tensor.py
+++ tracing_tensor.py
@@ -33,4 +33,19 @@ It gets every elemnt out one at a time
 > /home/kraudy/tinygrad/tinygrad/tinygrad/helpers.py(40)fully_flatten()->[1.0, 2.0, 3.0, 4.0]
 -> return flattened
 
+After that there some operatios to get the data type
+
+Looks like this genereates UOps
+> /home/kraudy/tinygrad/tinygrad/tinygrad/tensor.py(73)_frompy()
+-> def _frompy(x:Union[list, tuple, bytes], dtype:DType) -> UOp:
+
+After _frompy() it does to other options, i didn't know that even these explicit
+tensors where turned to Uops
+(Pdb) w
+  /home/kraudy/tinygrad/tinygrad-learn/tracing_tensor.py(4)<module>()
+-> a = Tensor([1., 2., 3., 4.])
+  /home/kraudy/tinygrad/tinygrad/tinygrad/tensor.py(165)__init__()
+-> else: data = _frompy(data, dtype)
+  /home/kraudy/tinygrad/tinygrad/tinygrad/tensor.py(76)_frompy()
+
 """
