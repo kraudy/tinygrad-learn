11d97851cf1a11869ff3ec29d9c18a6f07e6eb43
Roberto Torrez <robkraudy@gmail.com>
2025-04-14 07:21:35 -0600

    Found operation path
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- tracing_tensor.py
+++ tracing_tensor.py
@@ -3,4 +3,34 @@ from tinygrad import Tensor, nn
 
 a = Tensor([1., 2., 3., 4.])
 
+"""
+This data can be Numeric or UOp
+(Pdb) data
+[1.0, 2.0, 3.0, 4.0]
 
+> /home/kraudy/tinygrad/tinygrad/tinygrad/device.py(21)canonicalize()->'CLANG'
+-> def canonicalize(self, device:Optional[str]) -> str: return self._canonicalize(device) if device is not None else Device.DEFAULT
+
+Here is the thing, since data is a Union of multiple types, that is why we need to
+check isinstances for every data type afterwards
+
+On this case it enters
+> /home/kraudy/tinygrad/tinygrad/tinygrad/tensor.py(160)__init__()
+-> elif isinstance(data, (list, tuple)):
+(Pdb) --KeyboardInterrupt--
+(Pdb) isinstance(data, (list, tuple))
+
+> /home/kraudy/tinygrad/tinygrad/tinygrad/helpers.py(35)fully_flatten()
+-> def fully_flatten(l):
+
+> /home/kraudy/tinygrad/tinygrad/tinygrad/helpers.py(41)fully_flatten()->[1.0]
+> /home/kraudy/tinygrad/tinygrad/tinygrad/helpers.py(41)fully_flatten()->[2.0]
+> /home/kraudy/tinygrad/tinygrad/tinygrad/helpers.py(41)fully_flatten()->[3.0]
+> /home/kraudy/tinygrad/tinygrad/tinygrad/helpers.py(41)fully_flatten()->[4.0]
+
+It gets every elemnt out one at a time
+
+> /home/kraudy/tinygrad/tinygrad/tinygrad/helpers.py(40)fully_flatten()->[1.0, 2.0, 3.0, 4.0]
+-> return flattened
+
+"""
