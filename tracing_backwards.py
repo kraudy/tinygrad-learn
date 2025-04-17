afd6b3837671919df0c59a6394b245812f7421c8
Roberto Torrez <robkraudy@gmail.com>
2025-04-17 17:26:03 -0600

    Backwards toposort
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- tracing_backwards.py
+++ tracing_backwards.py
@@ -3,3 +3,81 @@ from tinygrad import Tensor
 
 t = Tensor([1.0, 2.0, 3.0, 4.0], requires_grad=True)
 t.sum().backward()
+
+"""
+(Pdb) w
+  /home/kraudy/tinygrad/tinygrad-learn/tracing_backwards.py(5)<module>()
+-> t.sum().backward()
+  /home/kraudy/tinygrad/tinygrad/tinygrad/tensor.py(929)backward()
+-> all_uops = self.lazydata.toposort
+  /home/kraudy/tinygrad/tinygrad/tinygrad/ops.py(282)toposort()
+-> return _toposort(self, cache=set())
+> /home/kraudy/tinygrad/tinygrad/tinygrad/ops.py(274)_toposort()
+-> def _toposort(u:UOp, cache:set[UOp]):
+
+
+
+(Pdb) p u
+UOp(Ops.COPY, dtypes.float, arg=False, src=(
+  UOp(Ops.DEVICE, dtypes.void, arg='CLANG', src=()),
+  UOp(Ops.BUFFER, dtypes.float, arg=(0, 4), src=(
+    UOp(Ops.DEVICE, dtypes.void, arg='PYTHON', src=()),)),))
+
+UOp(Ops.DEVICE, dtypes.void, arg='CLANG', src=())
+
+So, the toposort makes a dictionary with the operatios
+and uses a Set to keep track of visited.
+So, each _toposort() returns a dictionary with its Uop
+(Pdb) nodes
+{}
+
+Some nice recursion call stack
+(Pdb) w
+  /home/kraudy/tinygrad/tinygrad-learn/tracing_backwards.py(5)<module>()
+-> t.sum().backward()
+  /home/kraudy/tinygrad/tinygrad/tinygrad/tensor.py(929)backward()
+-> all_uops = self.lazydata.toposort
+  /home/kraudy/tinygrad/tinygrad/tinygrad/ops.py(282)toposort()
+-> return _toposort(self, cache=set())
+  /home/kraudy/tinygrad/tinygrad/tinygrad/ops.py(278)_toposort()
+-> for parent in u.src: nodes.update(_toposort(parent, cache))
+  /home/kraudy/tinygrad/tinygrad/tinygrad/ops.py(278)_toposort()
+-> for parent in u.src: nodes.update(_toposort(parent, cache))
+  /home/kraudy/tinygrad/tinygrad/tinygrad/ops.py(278)_toposort()
+-> for parent in u.src: nodes.update(_toposort(parent, cache))
+  /home/kraudy/tinygrad/tinygrad/tinygrad/ops.py(278)_toposort()
+-> for parent in u.src: nodes.update(_toposort(parent, cache))
+  /home/kraudy/tinygrad/tinygrad/tinygrad/ops.py(278)_toposort()
+-> for parent in u.src: nodes.update(_toposort(parent, cache))
+> /home/kraudy/tinygrad/tinygrad/tinygrad/ops.py(281)_toposort()->{UOp(Ops.DEVIC...THON', src=()): None}
+
+(Pdb) nodes
+{UOp(Ops.DEVICE, dtypes.void, arg='PYTHON', src=()): None, UOp(Ops.BUFFER, dtypes.float, arg=(0, 4), src=(
+  UOp(Ops.DEVICE, dtypes.void, arg='PYTHON', src=()),)): None}
+
+  (Pdb) nodes
+{UOp(Ops.DEVICE, dtypes.void, arg='CLANG', src=()): None, UOp(Ops.DEVICE, dtypes.void, arg='PYTHON', src=()): None, UOp(Ops.BUFFER, dtypes.float, arg=(0, 4), src=(
+  UOp(Ops.DEVICE, dtypes.void, arg='PYTHON', src=()),)): None}
+
+(Pdb) nodes
+{UOp(Ops.DEVICE, dtypes.void, arg='CLANG', src=()): None, UOp(Ops.DEVICE, dtypes.void, arg='PYTHON', src=()): None, UOp(Ops.BUFFER, dtypes.float, arg=(0, 4), src=(
+  UOp(Ops.DEVICE, dtypes.void, arg='PYTHON', src=()),)): None, UOp(Ops.COPY, dtypes.float, arg=False, src=(
+  UOp(Ops.DEVICE, dtypes.void, arg='CLANG', src=()),
+  UOp(Ops.BUFFER, dtypes.float, arg=(0, 4), src=(
+    UOp(Ops.DEVICE, dtypes.void, arg='PYTHON', src=()),)),)): None}
+
+(Pdb) nodes
+{UOp(Ops.DEVICE, dtypes.void, arg='CLANG', src=()): None, UOp(Ops.DEVICE, dtypes.void, arg='PYTHON', src=()): None, UOp(Ops.BUFFER, dtypes.float, arg=(0, 4), src=(
+  UOp(Ops.DEVICE, dtypes.void, arg='PYTHON', src=()),)): None, UOp(Ops.COPY, dtypes.float, arg=False, src=(
+  UOp(Ops.DEVICE, dtypes.void, arg='CLANG', src=()),
+  UOp(Ops.BUFFER, dtypes.float, arg=(0, 4), src=(
+    UOp(Ops.DEVICE, dtypes.void, arg='PYTHON', src=()),)),)): None}
+
+(Pdb) nodes
+{UOp(Ops.DEVICE, dtypes.void, arg='CLANG', src=()): None, UOp(Ops.DEVICE, dtypes.void, arg='PYTHON', src=()): None, UOp(Ops.BUFFER, dtypes.float, arg=(0, 4), src=(
+  UOp(Ops.DEVICE, dtypes.void, arg='PYTHON', src=()),)): None, UOp(Ops.COPY, dtypes.float, arg=False, src=(
+  UOp(Ops.DEVICE, dtypes.void, arg='CLANG', src=()),
+  UOp(Ops.BUFFER, dtypes.float, arg=(0, 4), src=(
+    UOp(Ops.DEVICE, dtypes.void, arg='PYTHON', src=()),)),)): None}
+
+"""
