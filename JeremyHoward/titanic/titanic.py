df9a054c28751a5ef37df6f2c458de1effd9afbc
Roberto Torrez <robkraudy@gmail.com>
2025-04-19 07:41:20 -0600

    Add optimizer
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- JeremyHoward/titanic/titanic.py
+++ JeremyHoward/titanic/titanic.py
@@ -14,6 +14,7 @@ print(X.head())
 
 Y = pd.read_csv("./train.csv", usecols=['Survived'])
 print(Y.head())
+# We need to make this [1, 0] or [0, 1] for alive and death
 
 """
 All columns
@@ -23,19 +24,31 @@ All columns
 
 x_cols = len(X.columns)
 """6"""
-y_cols = len(Y.columns)
-"""1"""
+classes = 2
 
 X = Tensor(X.values)
-Y = Tensor(Y.values)
+Y = Tensor(Y.values).one_hot(classes)
 """Tensfor from 'numpy.ndarray'"""
 
 L1_neurons = 24 #make it 100
 W1 = Tensor.randn(x_cols, L1_neurons)
 b1 = Tensor.randn(L1_neurons)
 
-L2_neurons = y_cols
-W2 = Tensor.randn(L1_neurons, L1_neurons)
-b2 = Tensor.randn(L1_neurons)
+L2_neurons = 2
+W2 = Tensor.randn(L1_neurons, L2_neurons)
+b2 = Tensor.randn(L2_neurons)
+"""Defined network"""
+
+params = [X, W1, b1, W2, b2]
+lr = 0.1 #validate
+optim = nn.optim.SGD(params, lr)
+"""Optimizer"""
 
 logits = X.matmul(W1).add(b1).tanh() # check relu, sigmoid, etc
+loss = logits.matmul(W2).add(b2).cross_entropy(Y)
+
+#zero grads
+
+#backward
+
+# update
