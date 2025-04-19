3a8489916f8a18c788d2fcdd5d7055759c335783
Roberto Torrez <robkraudy@gmail.com>
2025-04-18 19:15:58 -0600

    Some data cleaning
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- JeremyHoward/titanic/titanic.py
+++ JeremyHoward/titanic/titanic.py
@@ -5,7 +5,18 @@ from tinygrad import Tensor, nn
 import numpy as np
 import pandas as pd
 
-X = pd.read_csv("./train.csv")
-
+X = pd.read_csv("./train.csv", usecols=['PassengerId', 'Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare'])
+X['Sex'] = X['Sex'].replace({'male':1, "female":2})
 print(X.head())
+
+Y = pd.read_csv("./train.csv", usecols=['Survived'])
+print(Y.head())
+
+print(type(Y.values))
+
+"""
+All columns
+['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp',
+       'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
+"""
 
