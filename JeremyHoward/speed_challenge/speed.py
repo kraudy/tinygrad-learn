4e25abac08c4d155306987217c2990e1269f3114
Roberto Torrez <robkraudy@gmail.com>
2025-04-22 06:30:37 -0600

    Hsv conversion
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- JeremyHoward/speed_challenge/speed.py
+++ JeremyHoward/speed_challenge/speed.py
@@ -1,6 +1,7 @@
 from tinygrad import Tensor, nn
 import cv2
 import pdb
+import numpy as np
 """This is intalled with opencv-python"""
 
 
@@ -68,6 +69,10 @@ while capture.isOpened():
   flow = cv2.calcOpticalFlowFarneback(prev_gray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
 
   # Convert to HSV for training
-
+  mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+  hsv = np.zeros_like(prev)
+  hsv[..., 1] = 255
+  hsv[..., 0] = ang * 180 / np.pi / 2
+  hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
 
 capture.release()
