871509c573824adea21f0b4690492c829c47140e
Roberto Torrez <robkraudy@gmail.com>
2025-04-24 13:30:53 -0600

    Reading processed data
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- JeremyHoward/speed_challenge/speed.py
+++ JeremyHoward/speed_challenge/speed.py
@@ -1,3 +1,17 @@
 from tinygrad import Tensor, nn
 import numpy as np
+import pdb
+
+cache_path="./data/flow_images.npy"
+
+X = np.load(cache_path)
+"""
+  ...,
+  [ 94, 255,   0],
+  [ 94, 255,   0],
+  [ 94, 255,   0]]]], shape=(20399, 480, 640, 3), dtype=uint8)
+"""
+
+pdb.set_trace()
+Y = np.array([float(line) for line in open("./data/train.txt")])[: -1]
 
