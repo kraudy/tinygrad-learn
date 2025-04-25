79f9a7cfad01979418aeb27e771509033cb81293
Roberto Torrez <robkraudy@gmail.com>
2025-04-25 11:43:40 -0600

    Input data to Tensor
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- JeremyHoward/speed_challenge/speed.py
+++ JeremyHoward/speed_challenge/speed.py
@@ -3,7 +3,8 @@ import numpy as np
 import pdb
 
 cache_path="./data/flow_images.npy"
-
+ 
+print(f"Loading X")
 X = np.load(cache_path)
 """
   ...,
@@ -12,6 +13,17 @@ X = np.load(cache_path)
   [ 94, 255,   0]]]], shape=(20399, 480, 640, 3), dtype=uint8)
 """
 
-pdb.set_trace()
-Y = np.array([float(line) for line in open("./data/train.txt")])[: -1]
+#pdb.set_trace()
 
+print("Loading Y")
+Y = np.array([float(line) for line in open("./data/train.txt")])[: -1]
+"""
+array([28.105569, 28.105569, 28.106527, ...,  2.289795,  2.292917,
+2.2606  ], shape=(20399,))
+"""
+
+# Normalizing
+X = X / 255
+
+X = Tensor(X, dtype='float32')
+Y = Tensor(Y, dtype='float32')
