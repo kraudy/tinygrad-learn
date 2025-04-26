d97769d4ac089f3f20d2f78a3663c2da67a4888f
Roberto Torrez <robkraudy@gmail.com>
2025-04-26 06:39:15 -0600

    Better learning rate
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- JeremyHoward/speed_challenge/speed.py
+++ JeremyHoward/speed_challenge/speed.py
@@ -21,19 +21,22 @@ array([28.105569, 28.105569, 28.106527, ...,  2.289795,  2.292917,
 """
 
 print(f"Defining network")
-W1 = Tensor.randn(480*640*3, 300) * (1 / 480*640*3) ** 0.5
-b1 = Tensor.zeros(300)
+W1 = Tensor.randn(480*640*3, 400) * (1 / 480*640*3) ** 0.5
+b1 = Tensor.zeros(400)
 
-W2 = Tensor.randn(300, 100) * (1 / 300) ** 0.5
-b2 = Tensor.zeros(100)
+W2 = Tensor.randn(400, 200) * (1 / 400) ** 0.5
+b2 = Tensor.zeros(200)
 
-W3 = Tensor.randn(100, 1) * (1 / 100) ** 0.5
-b3 = Tensor.zeros(1)
+W3 = Tensor.randn(200, 100) * (1 / 200) ** 0.5
+b3 = Tensor.zeros(100)
+
+W4 = Tensor.randn(100, 1) * (1 / 100) ** 0.5
+b4 = Tensor.zeros(1)
 
 print(f"Declaring optimizer")
-params = [W1, b1, W2, b2]
+params = [W1, b1, W2, b2, W3, b3, W4, b4]
 
-lr = 0.1
+lr = 0.01
 optim = nn.optim.SGD(params, lr)
 
 # Load size
@@ -66,7 +69,7 @@ for chunk_X, chunk_Y in load_data_in_chunks():
     Y_batch = chunk_Y[batch_idx]
 
     print("Doing forward")
-    pred = X_batch.flatten(1).matmul(W1).add(b1).tanh().matmul(W2).add(b2).tanh().matmul(W3).add(b3)
+    pred = X_batch.flatten(1).matmul(W1).add(b1).tanh().matmul(W2).add(b2).tanh().matmul(W3).add(b3).tanh().matmul(W4).add(b4)
     loss = pred.sub(Y_batch).square().mean() # MSE Loss
     #loss = loss_fn(pred, Y_batch)
     optim.zero_grad()
@@ -181,5 +184,85 @@ Loss: 153.54296875
 Doing forward
 Loss: 149.7224884033203
 
+==================
+4 Layer deep network
+
+Declaring optimizer
+Training
+Loading data in chunks
+Doing forward
+Loss: 446.0902099609375
+Doing forward
+Loss: 13.301798820495605
+Doing forward
+Loss: 10307.1513671875
+Doing forward
+Loss: 223622.25
+Doing forward
+Loss: 57503032.0
+Doing forward
+Loss: 15093360640.0
+
+==================
+Reduce learning rate to 0.01 and got better results
+
+Loss: 457.8882751464844
+Doing forward
+Loss: 234.8868408203125
+Doing forward
+Loss: 89.94364166259766
+Doing forward
+Loss: 76.10680389404297
+Doing forward
+Loss: 58.915340423583984
+Doing forward
+Loss: 52.985084533691406
+Doing forward
+Loss: 51.70574951171875
+Doing forward
+Loss: 49.62165451049805
+Doing forward
+Loss: 36.65394973754883
+Doing forward
+Loss: 26.314184188842773
+Doing forward
+Loss: 27.996845245361328
+Doing forward
+Loss: 36.57184982299805
+Doing forward
+Loss: 29.273866653442383
+Doing forward
+Loss: 24.36199188232422
+Doing forward
+Loss: 21.98240852355957
+Doing forward
+Loss: 24.80967903137207
+Doing forward
+Loss: 17.632614135742188
+Doing forward
+Loss: 13.12913703918457
+Doing forward
+Loss: 12.841031074523926
+Doing forward
+Loss: 7.322067737579346
+Doing forward
+Loss: 16.83162498474121
+Doing forward
+Loss: 16.8055419921875
+Doing forward
+Loss: 16.514108657836914
+Doing forward
+Loss: 12.736820220947266
+Doing forward
+Loss: 20.2390079498291
+Doing forward
+Loss: 25.58208465576172
+Doing forward
+Loss: 38.64585494995117
+Doing forward
+Loss: 30.62093734741211
+Doing forward
+
+Now the Goal is to get the loss <= 10 using only a linear network
 
 """
