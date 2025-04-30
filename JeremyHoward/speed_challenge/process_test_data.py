96f9ad7983f47d13be490e17cb5187c6e7b4f366
Roberto Torrez <robkraudy@gmail.com>
2025-04-29 21:45:19 -0600

    Create zero array
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- JeremyHoward/speed_challenge/process_test_data.py
+++ JeremyHoward/speed_challenge/process_test_data.py
@@ -3,19 +3,50 @@ import pdb
 import numpy as np
 """This is intalled with opencv-python"""
 
-pdb.set_trace()
-
 capture = cv2.VideoCapture('./data/test.mp4')
+"""< cv2.VideoCapture 0x7fb6672551b0>"""
 
 ret, prev = capture.read()
-
+"""
+(Pdb) ret
+True
+(Pdb) prev
+array([[[6, 1, 0],
+        [6, 1, 0],
+        [6, 1, 0],
+        ...,
+        [4, 1, 2],
+        [4, 1, 2],
+        [4, 1, 2]]], shape=(480, 640, 3), dtype=uint8)
+"""
 prev_gray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
+"""
+(Pdb) prev_gray
+array([[1, 1, 1, ..., 0, 0, 0],
+       [2, 2, 2, ..., 0, 0, 0],
+       [2, 2, 2, ..., 0, 0, 0],
+       ...,
+       [1, 1, 1, ..., 2, 2, 2],
+       [1, 1, 1, ..., 2, 2, 2],
+       [1, 1, 1, ..., 2, 2, 2]], shape=(480, 640), dtype=uint8)
+"""
 
 crop_h=480
 crop_w=640
 
 frame_count = int(capture.get(cv2.CAP_PROP_FRAME_COUNT)) - 1  # xxx flow images
+"""10797"""
 test_flow_images = np.zeros((frame_count, crop_h, crop_w, 3), dtype=np.uint8)
+"""
+(Pdb) test_flow_images
+array([[[
+         ...,
+         [0, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0]]]], shape=(10797, 480, 640, 3), dtype=uint8)
+"""
+
+pdb.set_trace()
 
 for i in range(frame_count):
   ret, frame = capture.read()
