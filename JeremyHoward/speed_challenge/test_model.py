3789f0fd23b3fe5db6a54b3378b5d114b71f36d0
Roberto Torrez <robkraudy@gmail.com>
2025-05-01 16:06:31 -0600

    Define linear model
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- JeremyHoward/speed_challenge/test_model.py
+++ JeremyHoward/speed_challenge/test_model.py
@@ -1,8 +1,35 @@
 from tinygrad import Tensor, nn
 import numpy as np
 
+class Linear_Model():
+  def __init__(self):
+    # This first layer is kinda big
+    self.W1 = Tensor.randn(480*640*3, 600) * (1 / 480*640*3) ** 0.5
+    self.b1 = Tensor.zeros(600)
+
+    self.W2 = Tensor.randn(600, 400) * (1 / 600) ** 0.5
+    self.b2 = Tensor.zeros(400)
+
+    self.W3 = Tensor.randn(400, 200) * (1 / 400) ** 0.5
+    self.b3 = Tensor.zeros(200)
+
+    self.W4 = Tensor.randn(200, 100) * (1 / 200) ** 0.5
+    self.b4 = Tensor.zeros(100)
+
+    self.W5 = Tensor.randn(100, 1) * (1 / 100) ** 0.5
+    self.b5 = Tensor.zeros(1)
+
+  def __call__(self, X: Tensor) -> Tensor:
+    X = X.flatten(1)
+    X = X.matmul(self.W1).add(self.b1).tanh().dropout()
+    X = X.matmul(self.W2).add(self.b2).tanh().dropout()
+    X = X.matmul(self.W3).add(self.b3).tanh().dropout()
+    X = X.matmul(self.W4).add(self.b4).tanh().dropout()
+    X = X.matmul(self.W5).add(self.b5)
+    return X
+
 # Instantiate the model
-#model = Model()
+model = Linear_Model()
 
 # Load the saved model
 model_save_path = "./models/linear_sgd.npz"
