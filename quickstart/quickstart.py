dafa9a56d79918823cd8161420267a98fdc83f0a
Roberto Torrez <robkraudy@gmail.com>
2025-05-08 06:22:03 -0600

    Some refacto
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- quickstart/quickstart.py
+++ quickstart/quickstart.py
@@ -1,11 +1,17 @@
-from tinygrad import Linear
+from tinygrad import Tensor
+from tinygrad import nn
+
 
 class TinyNet:
   def __init__(self):
-    self.l1 = Linear(784, 128, bias=False)
-    self.l2 = Linear(128, 10, bias=False)
+    self.l1 = nn.Linear(784, 128, bias=False)
+    self.l2 = nn.Linear(128, 10, bias=False)
 
-  def __call__(self, x):
+  def __call__(self, x: Tensor) -> Tensor:
     x = self.l1(x).leaky_relu()
     x = self.l2(x)
     return x
+  
+net = TinyNet()
+
+optim = nn.optim.SGD([net.l1.weight, net.l2.weight], lr=3e-4)
